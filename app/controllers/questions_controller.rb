3604f084e3f280914b238f0f5ea6e9031b3c698c
Oleksandr Shliakhovyi <Oleksandr_Shliakhovyi@epam.com>
2023-01-12 18:36:39 +0200

    Add Question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,28 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new question_params
+
+    if @question.save
+      redirect_to questions_path
+      flash[:notice] = 'Post successfully created'
+    else
+      render :new
+      flash[:notice] = 'Not created'
+    end
+
+    
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get '/questions', to: 'questions#index'
 
+  get '/questions', to: 'questions#index'
+  get '/questions/new', to: 'questions#new'
+  post 'questions', to: 'questions#create'
 end
